b102780b4896cefd49b4621b8fc141f30d524ca6
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-05-07 12:59:32 +0200

    Update Validations.py

--- Validations.py
+++ Validations.py
@@ -6,36 +6,37 @@ class HashGenerator(object):
     def __init__(self, file):
         self.file = file
         self.BUF_SIZE = 65536
+        self.file_name = str(file.split("/")[-1])
         try:
             drive = str(file.split("/")[0])
-            file_name = str(file.split("/")[-1])
-            file_path = os.path.join(file.strip(file_name))
+            file_path = os.path.join(file.strip(self.file_name))
             if str(file_path.split("/")[0]) != drive:
                 file_path = str(drive.strip(":") + file_path)
             os.chdir(file_path)
         except Exception as E:
             print(E)
 
-    def generate(self):
-        
-
 
 class MD5(HashGenerator):
+    def __init__(self, *args, **kwargs):
+        super(MD5, self).__init__(*args, **kwargs)
+        self.md5_sum = hashlib.md5()
+        self.generate()
 
-
-
-    def hash_generator(self):
-        drive = str(self.check_file.split("\\")[0])
-        file_name = str(self.check_file.split("\\")[-1])
-        file_path = os.path.join(self.check_file.strip(file_name))
-        if str(file_path.split("\\")[0]) != drive:
-            file_path = str(drive.strip(":") + file_path)
-        os.chdir(file_path)
-        with open(file_name, 'rb') as file:
+    def generate(self):
+        with open(self.file_name, "rb") as file:
             while True:
                 data = file.read(self.BUF_SIZE)
                 if not data:
                     break
-                self.md5.update(data)
-                self.sha1.update(data)
-                self.sha256.update(data)
+                self.md5_sum.update(data)
+
+    def __str__(self):
+        return str(self.md5_sum.hexdigest())
+
+
+if __name__ == "__main__":
+    print("Drag 'n Drop File Here:")
+    file = str(input(r"  >> ")).replace('"', "")
+    test = MD5(file)
+    print(test)
