7b6aac9c212ff12ea734a02cd4f26fbc41bc8079
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-07-07 07:09:25 +0200

    Update Tetris.py

--- Games/Tetris.py
+++ Games/Tetris.py
@@ -10,19 +10,16 @@ if not os.path.isfile("scores.txt"):
         
 pygame.font.init()
 
-# GLOBALS VARS
 s_width = 800
 s_height = 700
-play_width = 300  # meaning 300 // 10 = 30 width per block
-play_height = 600  # meaning 600 // 20 = 30 height per block
+play_width = 300
+play_height = 600
 block_size = 30
 
 top_left_x = (s_width - play_width) // 2
 top_left_y = s_height - play_height
 
-
 # SHAPE FORMATS
-
 S = [['.....',
       '.....',
       '..00.',
