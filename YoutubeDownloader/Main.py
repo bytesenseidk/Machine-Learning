f3694d776be9ff0455475754a1c26ab543b43aa8
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-07-20 06:35:30 +0200

    Add files via upload

--- YoutubeDownloader/Main.py
+++ YoutubeDownloader/Main.py
@@ -1,6 +1,7 @@
+from __future__ import unicode_literals
 import os
 import sys
-from DownloadMethods import Download
+import youtube_dl
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 from PyQt5.QtCore import QCoreApplication, QObject, QRunnable
@@ -137,8 +138,69 @@ class MainWindow(QMainWindow):
         self.label_done.setText("Download Done!")
 
 
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+def my_hook(d):
+    if d["status"] == "downloading":
+        print(d["_percent_str"])
+
+
+class Download(object):
+    def __init__(self, url, save_path, quality, playlist=False):
+        self.url = url
+        self.save_path = save_path
+        self.qualities = {"Best": "1411",
+                          "Semi": "320",
+                          "Worst": "128"}
+        self.quality = self.qualities[quality]
+        self.playlist = playlist
+
+    def mp3_download(self):
+        opts = {
+            "verbose": False,
+            "fixup"  : "detect_or_warn",
+            "format" : "bestaudio/best",
+            "postprocessors" : [{
+                "key": "FFmpegExtractAudio",
+                "preferredcodec"  : "mp3",
+                "preferredquality": self.quality
+            }],
+            "logger": MyLogger(),
+            "extractaudio": True,
+            "process_hooks": [my_hook],
+            "outtmpl"     : self.save_path + "/%(title)s.%(ext)s",
+            "noplaylist"  : self.playlist
+        }
+        download_object = youtube_dl.YoutubeDL(opts)
+        download_object.download([self.url])
+
+    def mp4_download(self):
+        opts = {
+            "verbose": False,
+            "fixup"  : "detect_or_warn",
+            "format" : "bestaudio/best",
+            "postprocessors" : [{
+                "key": "FFmpegVideoConverter",
+                "preferredcodec"  : "mp4",
+            }],
+            "outtmpl"     : self.save_path + "/%(title)s.%(ext)s",
+            "noplaylist"  : self.playlist
+        }
+        download_object = youtube_dl.YoutubeDL(opts)
+        download_object.download([self.url])
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
+
