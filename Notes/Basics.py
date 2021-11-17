b0855d2a95927ccccb3f4ca44a20f18e27ec6f1c
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-11-17 14:30:30 +0100

    Add files via upload

---
+++ Notes/Basics.py
@@ -0,0 +1,7 @@
+""" Calculate the output """
+inputs = [1.2, 2.1, 3.7]
+weights = [7.1, 3.4, 4.1]
+bias = 3
+output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias
+
+print(output)
