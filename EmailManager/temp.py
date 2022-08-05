daf88afcb8160dada9261434a66caeff150008a8
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2022-08-05 15:14:43 +0200

    Mail Servers

---
+++ EmailManager/temp.py
@@ -0,0 +1,15 @@
+mail_servers = {
+    "gmail": [["smtp.gmail.com", 587], ["imap.gmail.com", 993]],
+    "aol": [["smtp.aol.com", 587], ["imap.aol.com", 993]],
+    "outlook": [["smtp-mail.outlook.com", 587], ["imap-mail.outlook.com", 993]],
+    "bt-connect": [["smtp.btconnect.com", 25], ["imap4.btconnect.com", 143]],
+    "office365": [["smtp.office365.com", 587], ["outlook.office365.com", 993]],
+    "yahoo": [["smtp.mail.yahoo.com", 465], ["imap.mail.yahoo.com", 993]],
+    "yahoo-plus": [["plus.smtp.mail.yahoo.com", 465], ["plus.imap.mail.yahoo.com", 993]],
+    "yahoo-uk": [["smtp.mail.yahoo.co.uk", 465], ["imap.mail.yahoo.co.uk", 993]],
+    "yahoo-europe": [["smtp.mail.yahoo.com", 465], ["imap.mail.yahoo.com", 993]],
+    "t-online": [["securesmtp.t-online.de", 587], ["secureimap.t-online.de", 993]],
+    "verizon": [["outgoing.verizon.net", 587], ["incoming.verizon.net", 143]]
+    # Find additional mail servers at https://www.systoolsgroup.com/imap/
+}
+
