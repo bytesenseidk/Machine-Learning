e20215f0c136d0c8a3a786083e38513ae9376308
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2022-08-06 17:41:41 +0200

    Clean Up

--- EmailManager/LoginScreen.py
+++ EmailManager/LoginScreen.py
@@ -3,7 +3,6 @@ import Login
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
-
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -11,18 +10,16 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Email Manager | Login")
         self.initUi()
         
-        
     def initUi(self):
         self.top_label = QtWidgets.QLabel(self)
         self.top_label.setObjectName("top_label")
         self.top_label.setGeometry(QtCore.QRect(0, 0, 800, 100))
-        font = QtGui.QFont()
-        font.setPointSize(22)
-        font.setBold(True)
-        font.setWeight(75)
-        self.top_label.setFont(font)
+        title_font = QtGui.QFont()
+        title_font.setPointSize(22)
+        title_font.setBold(True)
+        title_font.setWeight(75)
+        self.top_label.setFont(title_font)
         self.top_label.setLayoutDirection(QtCore.Qt.LeftToRight)
-        self.top_label.setStyleSheet("")
         self.top_label.setAlignment(QtCore.Qt.AlignCenter)
         self.top_label.setText("Email Manager")
         
@@ -34,7 +31,6 @@ class MainWindow(QMainWindow):
         label_font.setBold(True)
         label_font.setWeight(75)
         self.email_label.setFont(label_font)
-        self.email_label.setStyleSheet("")
         self.email_label.setAlignment(QtCore.Qt.AlignCenter)
         self.email_label.setText("Email")
         
@@ -46,9 +42,7 @@ class MainWindow(QMainWindow):
         input_font.setBold(True)
         input_font.setWeight(50)
         self.email_input.setFont(input_font)
-        self.email_input.setStyleSheet("")
         self.email_input.setMaxLength(100)
-        self.email_input.setPlaceholderText("")
         
         self.password_label = QtWidgets.QLabel(self)
         self.password_label.setObjectName("password_label")
@@ -63,17 +57,16 @@ class MainWindow(QMainWindow):
         self.password_input.setGeometry(QtCore.QRect(200, 230, 400, 30))
         self.password_input.setFont(input_font)
         self.password_input.setMaxLength(100)
-        self.password_input.setPlaceholderText("")
         self.password_input.setEchoMode(QtWidgets.QLineEdit.Password)
         
         self.signin_button = QtWidgets.QPushButton(self)
         self.signin_button.setObjectName("signin_button")
         self.signin_button.setGeometry(QtCore.QRect(350, 280, 100, 50))
-        font3 = QtGui.QFont()
-        font3.setPointSize(18)
-        font3.setBold(True)
-        font3.setWeight(75)
-        self.signin_button.setFont(font3)
+        button_font = QtGui.QFont()
+        button_font.setPointSize(18)
+        button_font.setBold(True)
+        button_font.setWeight(75)
+        self.signin_button.setFont(button_font)
         self.signin_button.setText("Login")
         self.signin_button.clicked.connect(self.login)
         
@@ -81,9 +74,8 @@ class MainWindow(QMainWindow):
         self.status_label.setObjectName("status_label")
         self.status_label.setGeometry(QtCore.QRect(245, 400, 300, 30))
         self.status_label.setFont(label_font)
-        self.status_label.setStyleSheet("")
         self.status_label.setAlignment(QtCore.Qt.AlignCenter)
-    
+        self.status_label.setFont(label_font)
     
     def login(self):
         email = self.email_input.text()
