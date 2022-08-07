daf54cb054e4f35222cd2fcd5867bd72d0e15965
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2022-08-07 20:22:25 +0200

    Clean Up

--- LoginApp/LoginApp.py
+++ LoginApp/LoginApp.py
@@ -1,6 +1,6 @@
 import sys
 import Login
-from PyQt5 import QtCore, QtGui, QtWidgets
+from SignUp import SignUp
 from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
@@ -8,7 +8,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 class LoginScreen(QMainWindow):
     def __init__(self):
         super(LoginScreen, self).__init__()
-        loadUi('LoginScreen.ui', self)
+        loadUi('Screens/LoginScreen.ui', self)
         self.setWindowTitle("Login")
         self.login_button.clicked.connect(self.login)
         self.signup_button.clicked.connect(self.sign_up)
@@ -26,7 +26,8 @@ class LoginScreen(QMainWindow):
             else:
                 self.feedback_label.setText("Invalid Username or Password")
         except Exception as e:
-            self.feedback_label.setText(e)
+            print(e)
+            self.feedback_label.setText(str(e))
     
     def sign_up(self):
         self.close()
@@ -41,7 +42,7 @@ class WelcomeScreen(QMainWindow):
     def __init__(self, username):
         self.username = username.split('@')[0]
         super(WelcomeScreen, self).__init__()
-        loadUi('WelcomeScreen.ui', self)
+        loadUi('Screens/WelcomeScreen.ui', self)
         self.setWindowTitle("Email Manager | Welcome")
         self.welcome_label.setText(f"Welcome {self.username}")
         self.logout_button.clicked.connect(self.logout)
@@ -58,14 +59,27 @@ class WelcomeScreen(QMainWindow):
 class SignUpScreen(QMainWindow):
     def __init__(self):
         super(SignUpScreen, self).__init__()
-        loadUi('SignUpScreen.ui', self)
+        loadUi('Screens/SignUpScreen.ui', self)
         self.setWindowTitle("Sign Up")
         self.create_button.clicked.connect(self.sign_up)
         self.exit_button.clicked.connect(self.exit)
         self.login_button.clicked.connect(self.login)
         
     def sign_up(self):
-        pass
+        if SignUp.valid_username(self.username_input.text()):
+            if self.password_input.text() == self.confirm_password_input.text():
+                self.username = self.username_input.text()
+                self.password = self.password_input.text()
+                user = SignUp(self.username, self.password)
+                user.save()
+                self.close()
+                self.login_screen = LoginScreen()
+                self.login_screen.show()
+            else:
+                self.feedback_label.setText("Passwords do not match")
+        else:
+            self.feedback_label.setText("Invalid Username")
+            
     
     def login(self):
         self.close()
--- LoginApp/SignUp.py
+++ LoginApp/SignUp.py
@@ -1,3 +1,5 @@
+import os
+
 class SignUp(object):
     def __init__(self, username=None, password=None):
         self.username = username
@@ -5,15 +7,12 @@ class SignUp(object):
         self.user_id = self.account_count()
     
     def account_count(self):
-        with open("users.txt", "r") as file:
-            text = file.readlines()
-            try:
-                for index, line in enumerate(text):
-                    count = index
-                return count + 1
-            except:
-                return 0
-
+        if os.path.exists("users.txt"):
+            with open("users.txt", "r") as file:
+                return len(file.readlines()) + 1
+        else:
+            return 0
+        
     def save(self):
         with open("users.txt", "a") as file:
             file.write(f"{self.username},{self.password},{self.user_id}\n")
@@ -22,10 +21,13 @@ class SignUp(object):
     @staticmethod
     def valid_username(username):
         valid = True
-        with open("users.txt", "r") as file:
-            text = file.readlines()
-            for line in text:
-                for user in line.split("\n"):
-                    if username == user.split(",")[0]:
-                        valid = False
+        if os.path.exists("users.txt"):
+            with open("users.txt", "r") as file:
+                text = file.readlines()
+                for line in text:
+                    for user in line.split("\n"):
+                        if username == user.split(",")[0]:
+                            valid = False
+                            return valid
+                    
         return valid
