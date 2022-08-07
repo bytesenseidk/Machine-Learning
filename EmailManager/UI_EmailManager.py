d0639850d1199848c9f52ceae7afe64fbf1cf67b
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2022-08-07 16:38:20 +0200

    Logout Method added

--- EmailManager/UI_EmailManager.py
+++ EmailManager/UI_EmailManager.py
@@ -16,20 +16,15 @@ class LoginScreen(QMainWindow):
         password = self.password_input.text()
         try:
             user = Login.Login(self.email, password)
-            test = user.login()
-            if test:
-                self.feedback_label.setText("Login Successful")
+            success = user.login()
+            if success:
                 self.close()
-                self.goto_welcome()
+                self.welcome_screen = WelcomeScreen(self.email)
+                self.welcome_screen.show()
                 
         except Exception as e:
             self.feedback_label.setText("Invalid Email or Password")
             print(e)
-    
-    def goto_welcome(self):
-        self.hide()
-        self.welcome_screen = WelcomeScreen(self.email)
-        self.welcome_screen.show()
 
 class WelcomeScreen(QMainWindow):
     def __init__(self, username):
@@ -38,6 +33,12 @@ class WelcomeScreen(QMainWindow):
         loadUi('WelcomeScreen.ui', self)
         self.setWindowTitle("Email Manager | Welcome")
         self.welcome_label.setText(f"Welcome {self.username}")
+        self.logout_button.clicked.connect(self.logout)
+        
+    def logout(self):
+        self.close()
+        self.login_screen = LoginScreen()
+        self.login_screen.show()
         
 if __name__ == "__main__":
     app = QApplication(sys.argv)
