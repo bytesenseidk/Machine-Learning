c7fb31333029e51b474c2506e18f3fa693d3f4e7
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2022-08-07 16:33:03 +0200

    Welcome Screen integration

--- EmailManager/UI_EmailManager.py
+++ EmailManager/UI_EmailManager.py
@@ -9,19 +9,36 @@ class LoginScreen(QMainWindow):
         super(LoginScreen, self).__init__()
         loadUi('LoginScreen.ui', self)
         self.setWindowTitle("Email Manager | Login")
+        self.login_button.clicked.connect(self.login)
 
     def login(self):
-        email = self.email_input.text()
+        self.email = self.email_input.text()
         password = self.password_input.text()
         try:
-            user = Login.Login(email, password)
+            user = Login.Login(self.email, password)
             test = user.login()
             if test:
-                self.status_label.setText("Login Successful")
+                self.feedback_label.setText("Login Successful")
+                self.close()
+                self.goto_welcome()
+                
         except Exception as e:
-            self.status_label.setText("Invalid Email or Password")
+            self.feedback_label.setText("Invalid Email or Password")
             print(e)
-            
+    
+    def goto_welcome(self):
+        self.hide()
+        self.welcome_screen = WelcomeScreen(self.email)
+        self.welcome_screen.show()
+
+class WelcomeScreen(QMainWindow):
+    def __init__(self, username):
+        self.username = username.split('@')[0]
+        super(WelcomeScreen, self).__init__()
+        loadUi('WelcomeScreen.ui', self)
+        self.setWindowTitle("Email Manager | Welcome")
+        self.welcome_label.setText(f"Welcome {self.username}")
+        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = LoginScreen()
