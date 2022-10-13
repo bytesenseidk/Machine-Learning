36928f6473a0d4bad8593fd8503f590f719fd107
LarsRosenkilde <lars.rosenkilde95@gmail.com>
2022-10-13 10:38:17 +0000

    Random generated data

--- Keras/BasicKeras.py
+++ Keras/BasicKeras.py
@@ -8,7 +8,7 @@ np.random.seed(2018) # Setting seed for reproducibility
 train_data = np.random.random((1000, 3)) # Random dummy-train data for 1000 students
 test_data = np.random.random((500, 3))   # Random dummy-test data for 500
 
-labels = np.random.randint(2, size=(1000, 1)) # Dummy results for 100 students (pass or fail)
+labels = np.random.randint(2, size=(1000, 1)) # Dummy results for 1000 students (pass or fail)
 
 """ DEFINING MODEL STRUCTURE """
 model = Sequential()
@@ -17,5 +17,6 @@ model.add(Dense(4, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+""" TRAINING THE MODEL AND MAKING PREDICTIONS """
 model.fit(train_data, labels, epochs=10, batch_size=32) # Train model and predict
 predictions = model.predict(test_data) # Make predictions from the trained model
---
+++ Keras/KerasIris.py
@@ -0,0 +1,22 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.datasets import load_iris
+import numpy as np
+
+np.random.seed(2018) # Setting seed for reproducibility
+iris = load_iris()
+indices = np.random.permutation(len(iris.data))
+test_samples = 12
+
+train_data = iris.data[indices[:-test_samples]] 
+test_data = iris.data[indices[-test_samples:]]
+labels = iris.target[indices[:-test_samples]]
+
+model = Sequential()
+model.add(Dense(5, input_dim=4, activation='relu'))
+model.add(Dense(4, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.fit(train_data, labels, epochs=5, batch_size=32)
+predictions = model.predict(test_data)
